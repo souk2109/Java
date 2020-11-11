3375ed3fe9bd9e5c4babb7ebe814907d66a8027a
souk2109 <souk2109@naver.com>
2020-11-11 14:20:05 +0900

    primeNumber 판별 코딩 [2020-11-11]

---
+++ Algorithm/Solution/src/findPrimenumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class findPrimenumber {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input number : ");
+		int num = scan.nextInt();
+		
+		boolean IsPrimeNumber = true;
+		
+		if(num == 1 ) 
+		{
+			System.out.println("it is not primeNumber.");
+			IsPrimeNumber = false;
+		}
+		else if(num == 2)
+		{
+			System.out.println("it is primeNumber.");
+		}
+		else 
+		{
+			for(int i=2; i<=num/2; i++) 
+			{
+				int remainder = num % i;
+				if(remainder == 0) 
+				{
+					IsPrimeNumber = false;
+					break;
+				}
+			}
+
+			if(IsPrimeNumber == true)
+				System.out.println("it is primeNumber.");
+			else
+				System.out.print("it is not primeNumber.ÀÔ´Ï´Ù");	
+		}
+	}
+
+}
