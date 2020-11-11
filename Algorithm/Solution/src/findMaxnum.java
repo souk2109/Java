d79071d1f69cf8ad44939856d7b22e4f16a252cc
souk2109 <souk2109@naver.com>
2020-11-11 14:30:36 +0900

    수정

--- Algorithm/Solution/src/findMaxnum.java
+++ Algorithm/Solution/src/findMaxnum.java
@@ -4,7 +4,7 @@ public class findMaxnum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//	ㅇㅈㅇㅈㅈ
+
 		Scanner scan = new Scanner(System.in);
 		System.out.print("input number : ");
 		int num = scan.nextInt();	
