ac5cd874cec254ca4b8e32dad704e4cc808d7cb2
souk2109 <souk2109@naver.com>
2020-11-12 23:41:18 +0900

    인터페이스 구현 [2020-11-12]

--- Interface/Example/src/Calculator/CalculateInterface.java
+++ Interface/Example/src/Calculator/CalculateInterface.java
@@ -5,4 +5,5 @@ interface CalculateInterface {
 	public abstract void minus(float num1, float num2);
 	public abstract void multiplication(float num1, float num2);
 	public abstract void division(float num1, float num2);
+	
 }
